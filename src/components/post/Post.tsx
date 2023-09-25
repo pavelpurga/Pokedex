8eb775cb267e003ceb78cdc5ac534c759f428dee
Pablo.ovrf <pashapetrokas@gmail.com>
2023-09-25 13:32:59 +0300

    post styles improved

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -2,6 +2,7 @@ import React, {FC, useState} from 'react';
 import {Button, Card, Modal} from "antd";
 import PostDetails from "./PostDetails";
 import {PostsTypes} from "../../entitysData/models/Posts.types";
+import '../../styles/index.css';
 interface PostProps{
     post: PostsTypes,
     image: string | undefined;
@@ -22,15 +23,15 @@ const Post : FC<PostProps> = ({post,image}) => {
     <div style={{marginTop:15 ,display:"flex", justifyContent:"center"}}>
       <Card
         title={<p className="postTitle">{post.title}</p>}
-        style={{ width: 650 }}
+        style={{ width: 650, height: 225 }}
         className="post"
         hoverable={true}
         extra={<Button className="button_more_post" onClick={openModal}>More</Button>}
       >
         <div style={{display:"flex"}}>
           {image && <img src={image}  style={{width:80,height:130}} />}
-          <div style={{ marginLeft: 10, height: 100,width:500,columnCount: 1,overflow:"auto"}}>
-            {post.body}
+          <div style={{ marginLeft: 10, height: 100, width: 500}}>
+            <div className="postBody">{post.body}</div>
           </div>
         </div>
         <Modal
